68cfa41a89c101ec8f5e0b6770e3bf5180170127
JornEirikBetten <jorneirikbetten1@gmail.com>
2022-05-11 23:19:10 +0200

    Closing in on parallel implementation. Missing iso_diffusion_denoising_parallel.

--- parallel_code/main.c
+++ parallel_code/main.c
@@ -44,6 +44,7 @@ int main(int argc, char *argv[])
   int right = 1;
   float kappa;
   MPI_Comm comm_cart;
+  MPI_Status status;
   image u, u_bar, whole_image;
   unsigned char *image_chars, *my_image_chars;
   char *input_jpeg_filename, *output_jpeg_filename;
@@ -97,30 +98,84 @@ int main(int argc, char *argv[])
   MPI_Cart_shift(comm_cart, 0, 1, &rank_dirs[UP], &rank_dirs[DOWN]);
   MPI_Cart_shift(comm_cart, 1, 1, &rank_dirs[LEFT], &rank_dirs[RIGHT]);
   printf("P[%d]: neighbors(u,d,l,r)=%d %d %d %d\n",my_rank, rank_dirs[UP],rank_dirs[DOWN],rank_dirs[LEFT], rank_dirs[RIGHT]);
+
   my_mstart = coord[0]*(m-2)/4;
   my_mstop = (coord[0]+1)*(m-2)/4;
   my_m = my_mstop-my_mstart+2;
   my_nstart = coord[1]*(n-2)/3;
   my_nstop = (coord[1]+1)*(n-2)/3;
   my_n = my_nstop-my_nstart+2;
+  *my_image_chars = (unsigned char*)malloc(my_m*my_n*sizeof(unsigned char));
   printf("P[%d]: m: start: %d, stop: %d, my_m: %d\n", my_rank, my_mstart, my_mstop, my_m);
   printf("P[%d]: n: start: %d, stop: %d, my_n: %d\n", my_rank, my_nstart, my_nstop, my_n);
-  MPI_Finalize();
-  return 0;
-  /*
+
   allocate_image (&u, my_m, my_n);
   allocate_image (&u_bar, my_m, my_n);
-  */
   /* each process asks process 0 for a partitioned region */
   /* of image_chars and copy the values into u */
   /* ... */
-  //convert_jpeg_to_image (my_image_chars, &u);
-  //iso_diffusion_denoising_parallel (&u, &u_bar, kappa, iters);
+  if (my_rank>0) {
+    MPI_Send(&my_mstart, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+    MPI_Send(&my_m, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+    MPI_Send(&my_nstart, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+    MPI_Send(&my_n, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+    MPI_Recv(&my_image_chars, my_m*my_n, MPI_UNSIGNED_CHAR, 0, 0, MPI_COMM_WORLD, &status);
+  }
+  if (my_rank==0) {
+    int mstart, msize, nstart, nsize;
+    for (int i=1; i<12; i++) {
+      MPI_Recv(&mstart, 1, MPI_INT, i, 0, MPI_COMM_WORLD, &status);
+      MPI_Recv(&msize, 1, MPI_INT, i, 0, MPI_COMM_WORLD, &status);
+      MPI_Recv(&nstart, 1, MPI_INT, i, 0, MPI_COMM_WORLD, &status);
+      MPI_Recv(&nsize, 1, MPI_INT, i, 0, MPI_COMM_WORLD, &status);
+      unsigned char *chunk = (unsigned char*)malloc(nsize*msize*sizeof(unsigned char));
+      for (int i=mstart; i<mstart + msize; i++) {
+        for (int j=nstart; j<nstart + nsize; j++) {
+          chunk[(i-mstart)*nsize + (j-nstart)] = image_chars[i*n + j];
+        }
+      }
+      MPI_Send(&chunk, nsize*msize, MPI_UNSIGNED_CHAR, i, 0, MPI_COMM_WORLD);
+    }
+    for (int i=my_mstart; i<my_mstart + my_m; i++) {
+      for (int j=my_nstart; j<my_nstart+my_n; j++) {
+        my_image_chars[(i-my_mstart)*nsize + (j-nstart)] = image_chars[i*n + j];
+      }
+    }
+  }
+
+
+  convert_jpeg_to_image (my_image_chars, &u);
+  iso_diffusion_denoising_parallel (&u, &u_bar, kappa, iters);
   /* each process sends its resulting content of u_bar to process 0 */
   /* process 0 receives from each process incoming values and */
   /* copy them into the designated region of struct whole_image */
   /* ... */
-  /*
+  if (my_rank>0) {
+    MPI_Send(&my_m, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+    MPI_Send(&my_mstart, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+    MPI_Send(&my_n, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+    MPI_Send(&my_nstart, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+    MPI_Send(&u_bar.image_data, my_m*my_n, MPI_FLOAT, 0, 0, MPI_COMM_WORLD);
+  }
+  if (my_rank==0){
+    for (int i=1; i<12; i++) {
+      MPI_Recv(&msize, 1, MPI_INT, i, 0, MPI_COMM_WORLD, &status);
+      MPI_Recv(&mstart, 1, MPI_INT, i, 0, MPI_COMM_WORLD, &status);
+      MPI_Recv(&nsize, 1, MPI_INT, i, 0, MPI_COMM_WORLD, &status);
+      MPI_Recv(&nstart, 1, MPI_INT, i, 0, MPI_COMM_WORLD, &status);
+      float** temp = (float**)malloc(msize*sizeof(float*));
+      for (int i=0; i<msize; i++) {
+        *temp[i] = (float*)malloc(nsize*sizeof(float)); 
+      }
+      MPI_Recv(&temp, msize*nsize, MPI_FLOAT, i, 0, MPI_COMM_WORLD, &status);
+
+      for (int i=mstart+1; i<mstart + msize -1; i++){
+        for (int j=nstart+1; j<nstart + nsize -1; j++) {
+          whole_image.image_data[i][j] = temp[i-mstart][j-nstart];
+        }
+      }
+    }
+  }
   if (my_rank==0) {
     convert_image_to_jpeg(&whole_image, image_chars);
     export_JPEG_file(output_jpeg_filename, image_chars, m, n, c, 75);
@@ -128,7 +183,7 @@ int main(int argc, char *argv[])
   }
   deallocate_image (&u);
   deallocate_image (&u_bar);
-  */
-  //MPI_Finalize ();
-  //return 0;
+
+  MPI_Finalize ();
+  return 0;
 }
