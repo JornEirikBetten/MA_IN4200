e03c6a61920f64608017fa65ceff0ef1eec16bb7
JornEirikBetten <jorneirikbetten1@gmail.com>
2022-05-12 18:54:11 +0200

    print

--- parallel_code/main.c
+++ parallel_code/main.c
@@ -99,7 +99,7 @@ int main(int argc, char *argv[])
   m_array[num_procs-1] = m_slice + ((num_procs-1) >= (num_procs - remainder_slice) ? 1:0);
   counts[num_procs-1] = m_array[num_procs-1]*n;
   printf("counts[%d] = %d\n", num_procs-1, counts[num_procs-1]);
-  printf("displs[%d] = %d\n", displs[i]); 
+  printf("displs[%d] = %d\n", displs[num_procs-1]); 
   /* Allocating array for saving of number of rows in slices */
   if (my_rank==0 || my_rank==num_procs-1) {
     my_m = m_array[my_rank] + 1;
